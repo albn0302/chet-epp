de5b9133a4c4ef775cae0494501a9dd0dcdd19f1
albn0302 <albin.johansson0302@gmail.com>
2022-11-03 12:50:52 +0200

    Update 1.3

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-app.js";
 import { getDatabase, ref, onChildAdded, set } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-database.js"
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-auth.js";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -15,24 +16,55 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+// Authentication
+// -----------------------------------------------------------------
+// Set variable with Bootstrap modal
+const loginModal = new bootstrap.Modal('#login-modal');
+loginModal.show();
+
+// Listen for click on login button
+document.querySelector("#login-button").addEventListener("click", function login() {
+    const email = document.querySelector("#email").value;
+    const password = document.querySelector("#password").value;
+    const auth = getAuth();
+
+    // Sign in with Firebase
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            console.log(user);
+
+            // Hide modal
+            loginModal.hide();
+
+            // Call function to init database
+            initDatabase();
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+});
 
 // Initialize Realtime Database and get a referance to the service
 const db = getDatabase(app);
+function initDatabase() {
 
-// Create referance, where in the database
-const chatRef = ref(db, '/chat');
+    // Create referance, where in the database
+    const chatRef = ref(db, '/chat');
 
-// Listen to database changes
-onChildAdded(chatRef, function (data) {
+    // Listen to database changes
+    onChildAdded(chatRef, function (data) {
 
-    // Create message element and append to list element
-    const message = document.createElement("li");
-    message.innerText = new Date(data.key).toLocaleString("fi-FI") + " - " + data.val();
-    list.appendChild(message);
+        // Create message element and append to list element
+        const message = document.createElement("li");
+        message.innerText = new Date(data.key).toLocaleString("fi-FI") + " : " + data.val();
+        list.appendChild(message);
 
-});
+    });
+};
 
-// Chat
+// New message
 const input = document.querySelector("input");
 const list = document.querySelector("ul");
 
